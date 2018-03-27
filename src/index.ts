84725248dc80d7177dcf5fbd94166186dc1d458b
Paul Koerbitz <paul.koerbitz@gmail.com>
2018-03-27 23:23:50 +0200

    Remove unneeded code from index.ts
    Most code was copied over from tslint - a few options actually
    do not make sense in the tslint-auto-disable context.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-import commander = require("commander");
+import * as commander from "commander";
 import * as fs from "fs";
 import * as glob from "glob";
 import { filter as createMinimatchFilter, Minimatch } from "minimatch";
 import * as path from "path";
 import tsl, { Configuration, Linter, LintResult, Replacement, RuleFailure, Utils } from "tslint";
 import * as ts from "typescript";
-// import { run } from "./runner";
-// import { arrayify, dedent } from "./utils";
 
 const { findConfiguration } = Configuration;
 const { dedent, arrayify, flatMap } = Utils;
@@ -14,17 +12,10 @@ const { dedent, arrayify, flatMap } = Utils;
 interface Argv {
     config?: string;
     exclude: string[];
-    fix?: boolean;
-    force?: boolean;
     help?: boolean;
-    init?: boolean;
-    out?: string;
-    outputAbsolutePaths: boolean;
     project?: string;
     rulesDir?: string;
-    formattersDir: string;
     format?: string;
-    typeCheck?: boolean;
     test?: boolean;
     version?: boolean;
 }
@@ -32,7 +23,7 @@ interface Argv {
 interface Option {
     short?: string;
     // Commander will camelCase option names.
-    name: keyof Argv | "rules-dir" | "formatters-dir" | "type-check";
+    name: keyof Argv | "rules-dir";
     type: "string" | "boolean" | "array";
     describe: string; // Short, used for usage message
     description: string; // Long, used for `--help`
@@ -70,42 +61,6 @@ const options: Option[] = [
             globs to indicate which files to exclude.`,
     },
     {
-        name: "fix",
-        type: "boolean",
-        describe: "fixes linting errors for select rules (this may overwrite linted files)",
-        description: "Fixes linting errors for select rules. This may overwrite linted files.",
-    },
-    {
-        name: "force",
-        type: "boolean",
-        describe: "return status code 0 even if there are lint errors",
-        description: dedent`
-            Return status code 0 even if there are any lint errors.
-            Useful while running as npm script.`,
-    },
-    {
-        short: "i",
-        name: "init",
-        type: "boolean",
-        describe: "generate a tslint.json config file in the current working directory",
-        description: "Generates a tslint.json config file in the current working directory.",
-    },
-    {
-        short: "o",
-        name: "out",
-        type: "string",
-        describe: "output file",
-        description: dedent`
-            A filename to output the results to. By default, tslint outputs to
-            stdout, which is usually the console where you're running it from.`,
-    },
-    {
-        name: "outputAbsolutePaths",
-        type: "boolean",
-        describe: "whether or not outputted file paths are absolute",
-        description: "If true, all paths in the output will be absolute.",
-    },
-    {
         short: "r",
         name: "rules-dir",
         type: "string",
@@ -116,33 +71,6 @@ const options: Option[] = [
             node_modules/tslint/lib/rules, before checking the user-provided
             rules directory, so rules in the user-provided rules directory
             with the same name as the base rules will not be loaded.`,
-    },
-    {
-        short: "s",
-        name: "formatters-dir",
-        type: "string",
-        describe: "formatters directory",
-        description: dedent`
-            An additional formatters directory, for user-created formatters.
-            Formatters are files that will format the tslint output, before
-            writing it to stdout or the file passed in --out. The default
-            directory, node_modules/tslint/build/formatters, will always be
-            checked first, so user-created formatters with the same names
-            as the base formatters will not be loaded.`,
-    },
-    {
-        short: "t",
-        name: "format",
-        type: "string",
-        describe: "output format (prose, json, stylish, verbose, pmd, msbuild, checkstyle, vso, fileslist, codeFrame)",
-        description: dedent`
-            The formatter to use to format the results of the linter before
-            outputting it to stdout or the file passed in --out. The core
-            formatters are prose (human readable), json (machine readable)
-            and verbose. prose is the default if this option is not used.
-            Other built-in options include pmd, msbuild, checkstyle, and vso.
-            Additional formatters can be added and used if the --formatters-dir
-            option is set.`,
     },
     {
         name: "test",
@@ -164,14 +92,6 @@ const options: Option[] = [
             The path or directory containing a tsconfig.json file that will be
             used to determine which files will be linted. This flag also enables
             rules that require the type checker.`,
-    },
-    {
-        name: "type-check",
-        type: "boolean",
-        describe: "(deprecated) check for type errors before linting the project",
-        description: dedent`
-            (deprecated) Checks for type errors before linting a project.
-            --project must be specified in order to enable type checking.`,
     },
 ];
 
@@ -214,28 +134,16 @@ commander.on("--help", () => {
 
 // Hack to get unknown option errors to work. https://github.com/visionmedia/commander.js/pull/121
 const parsed = commander.parseOptions(process.argv.slice(2));
-commander.args = parsed.args;
+(commander as any).args = parsed.args;
 if (parsed.unknown.length !== 0) {
     (commander.parseArgs as (args: string[], unknown: string[]) => void)([], parsed.unknown);
 }
 const argv = commander.opts() as any as Argv;
 
-if (!(argv.init || argv.test !== undefined || argv.project !== undefined || commander.args.length > 0)) {
+if (!(argv.test !== undefined || argv.project !== undefined || commander.args.length > 0)) {
     console.error("No files specified. Use --project to lint a project folder.");
     process.exit(1);
 }
-
-if (argv.typeCheck) {
-    console.warn("--type-check is deprecated. You only need --project to enable rules which need type information.");
-    if (argv.project === undefined) {
-        console.error("--project must be specified in order to enable type checking.");
-        process.exit(1);
-    }
-}
-
-const outputStream: NodeJS.WritableStream = argv.out === undefined
-    ? process.stdout
-    : fs.createWriteStream(argv.out, { flags: "w+", mode: 420 });
 
 export interface Options {
     /**
@@ -254,41 +162,6 @@ export interface Options {
     files: string[];
 
     /**
-     * Whether to return status code 0 even if there are lint errors.
-     */
-    force?: boolean;
-
-    /**
-     * Whether to fixes linting errors for select rules. This may overwrite linted files.
-     */
-    fix?: boolean;
-
-    /**
-     * Output format.
-     */
-    format?: string;
-
-    /**
-     * Formatters directory path.
-     */
-    formattersDirectory?: string;
-
-    /**
-     * Whether to generate a tslint.json config file in the current working directory.
-     */
-    init?: boolean;
-
-    /**
-     * Output file path.
-     */
-    out?: string;
-
-    /**
-     * Whether to output absolute paths
-     */
-    outputAbsolutePaths?: boolean;
-
-    /**
      * tsconfig.json file.
      */
     project?: string;
@@ -304,11 +177,6 @@ export interface Options {
      * should be executed.
      */
     test?: boolean;
-
-    /**
-     * Whether to enable type checking when linting a project.
-     */
-    typeCheck?: boolean;
 }
 
 export const enum Status {
@@ -339,35 +207,31 @@ async function runWorker(options: Options, logger: Logger): Promise<Status> {
     if (output && output.trim()) {
         logger.log(`${output}\n`);
     }
-    return options.force || errorCount === 0 ? Status.Ok : Status.LintError;
+    return errorCount === 0 ? Status.Ok : Status.LintError;
 }
 
 async function runLinter(options: Options, logger: Logger): Promise<LintResult> {
     const { files, program } = resolveFilesAndProgram(options, logger);
     // if type checking, run the type checker
-    if (program && options.typeCheck) {
+    if (program) {
         const diagnostics = ts.getPreEmitDiagnostics(program);
         if (diagnostics.length !== 0) {
-            const message = diagnostics.map((d) => showDiagnostic(d, program, options.outputAbsolutePaths)).join("\n");
-            if (options.force) {
-                logger.error(`${message}\n`);
-            } else {
-                throw new Error(message);
-            }
+            const message = diagnostics.map((d) => showDiagnostic(d, program)).join("\n");
+            throw new Error(message);
         }
     }
     return doLinting(options, files, program, logger);
 }
 
 function resolveFilesAndProgram(
-    { files, project, exclude, outputAbsolutePaths }: Options,
+    { files, project, exclude }: Options,
     logger: Logger,
 ): { files: string[]; program?: ts.Program } {
     // remove single quotes which break matching on Windows when glob is passed in single quotes
     exclude = exclude.map(trimSingleQuotes);
 
     if (project === undefined) {
-        return { files: resolveGlobs(files, exclude, outputAbsolutePaths, logger) };
+        return { files: resolveGlobs(files, exclude, false, logger) };
     }
 
     const projectPath = findTsconfig(project);
@@ -426,9 +290,7 @@ function resolveGlobs(files: string[], ignore: string[], outputAbsolutePaths: bo
 async function doLinting(options: Options, files: string[], program: ts.Program | undefined, logger: Logger): Promise<LintResult> {
     const linter = new Linter(
         {
-            fix: !!options.fix,
-            formatter: options.format,
-            formattersDirectory: options.formattersDirectory,
+            fix: false,
             rulesDirectory: options.rulesDirectory,
         },
         program);
@@ -545,21 +407,13 @@ run(
         config: argv.config,
         exclude: argv.exclude,
         files: arrayify(commander.args),
-        fix: argv.fix,
-        force: argv.force,
-        format: argv.format === undefined ? "prose" : argv.format,
-        formattersDirectory: argv.formattersDir,
-        init: argv.init,
-        out: argv.out,
-        outputAbsolutePaths: argv.outputAbsolutePaths,
         project: argv.project,
         rulesDirectory: argv.rulesDir,
         test: argv.test,
-        typeCheck: argv.typeCheck,
     },
     {
         log(m) {
-            outputStream.write(m);
+            process.stdout.write(m);
         },
         error(m) {
             process.stdout.write(m);
