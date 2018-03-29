4b0fd475fa9fc0e19aaff331a4f60839e919d731
Paul Koerbitz <paul.koerbitz@gmail.com>
2018-03-29 08:03:15 +0200

    Test that indents are preserved for inserted disable comments

---
+++ test/cases/04-indent-depth-is-preserved/input.ts
@@ -0,0 +1,6 @@
+const foo = () => {
+    const quox: any = 1;
+    if (3 > 0) {
+        const baz: any = 2;
+    }
+}
---
+++ test/cases/04-indent-depth-is-preserved/output.ts
@@ -0,0 +1,8 @@
+const foo = () => {
+    // tslint:disable-next-line
+    const quox: any = 1;
+    if (3 > 0) {
+        // tslint:disable-next-line
+        const baz: any = 2;
+    }
+}
